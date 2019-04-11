c4540ab506837edeb4a644d0b65529fad012dde7
dhr1676 <32042158+dhr1676@users.noreply.github.com>
2019-04-11 17:03:26 -0400

    ding

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask import render_template
 import pymysql
+from flask_cors import CORS
+
 from PetInfoMasonry import PetInfoMasonry
 from PetInfoDetail import PetInfoDetail
 from PetInfoCreate import PetInfoCreate
@@ -10,6 +12,7 @@ from PetInfoUpdate import PetInfoUpdate
 from ShopDetail import ShopDetail
 
 app = Flask(__name__)
+# CORS(app, supports_credentials=True)
 api = Api(app)
 
 api.add_resource(PetInfoMasonry, "/PetInfoMasonry/")

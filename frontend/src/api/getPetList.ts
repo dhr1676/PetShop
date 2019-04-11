ae70a8a3b18ca59526a787a626baf9c44f6aefd3
nichena01024 <32244999+nichena01024@users.noreply.github.com>
2019-04-11 17:13:47 -0400

    fix fetch

--- frontend/src/api/getPetList.ts
+++ frontend/src/api/getPetList.ts
@@ -5,7 +5,7 @@ export function getPetList(index: number, len?: number = 10): void {
     fetch(`${option.base}/PetInfoMasonry?start_index=${index}&length=${len}`, {
         method: 'GET',
     }).then((res) => {
-        ws$.next(res)
+        ws$.next(res.body)
     }).catch((err) => {
         console.log('fuck you')
     })
